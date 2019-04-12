bce98ce6a09867730ba2b0076423e913da87b0b3
Campbell Barton <ideasman42@gmail.com>
2019-04-12 20:50:08 +1000

    clang-format: disable for DefNode
    This doesn't format properly and gives different results on second
    execution.

--- source/blender/nodes/NOD_static_types.h
+++ source/blender/nodes/NOD_static_types.h
@@ -20,6 +20,8 @@
 
 /* intentionally no include guard */
 
+/* clang-format off */
+
 /* Empty definitions for undefined macros to avoid warnings */
 #ifndef DefNode
 #define DefNode(Category, ID, DefFunc, EnumName, StructName, UIName, UIDesc)
@@ -249,3 +251,5 @@ DefNode(TextureNode,    TEX_NODE_PROC+TEX_DISTNOISE, 0,                  "TEX_DI
 
 /* undefine macros */
 #undef DefNode
+
+/* clang-format on */
