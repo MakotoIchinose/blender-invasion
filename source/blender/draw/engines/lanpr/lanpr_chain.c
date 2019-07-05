0f606437c3d886f39afd13aed92f8f20e89c307e
Ray Molenkamp <github@lazydodo.com>
2019-07-05 07:12:45 -0600

    Cleanup: Fix build error with MSVC
    space after \ is not allowed

--- source/blender/draw/engines/lanpr/lanpr_chain.c
+++ source/blender/draw/engines/lanpr/lanpr_chain.c
@@ -493,9 +493,7 @@ void lanpr_link_point_with_bounding_area_recursive(LANPR_RenderBuffer *rb,
     LANPR_BoundingArea *ch = root->child;
 
 #define IN_BOUND(ba, rlci) \
-  \ 
-      ba.l <= rlci->pos[0] && \
-      ba.r >= rlci->pos[0] && ba.b <= rlci->pos[1] && ba.u >= rlci->pos[1]
+  ba.l <= rlci->pos[0] && ba.r >= rlci->pos[0] && ba.b <= rlci->pos[1] && ba.u >= rlci->pos[1]
 
     if (IN_BOUND(ch[0], rlci)) {
       lanpr_link_point_with_bounding_area_recursive(rb, &ch[0], rlc, rlci);
