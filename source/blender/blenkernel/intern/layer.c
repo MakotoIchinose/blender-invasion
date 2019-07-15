5bb9fce0cdead9045506dc5dd87154a10f987a7d
Nathan Craddock <nzcraddock@gmail.com>
2019-07-15 13:29:42 -0600

    Collections: Update active collection after visibility restriction
    When changing viewport visiblity or restriction change the active
    collection. This replicates the behavior of excluding the active
    collection, which sets the first non-excluded parent as active.
    
    This required a function to recursively check parents for the
    visiblility restrictions.

--- source/blender/blenkernel/intern/layer.c
+++ source/blender/blenkernel/intern/layer.c
@@ -494,6 +494,38 @@ static LayerCollection *collection_from_index(ListBase *lb, const int number, in
 }
 
 /**
+ * Return true if the elements in a collection are visible
+ * Used to set active collection after excluding or restricting
+ * collection visiblility
+ */
+static bool layer_collection_hidden(ViewLayer *view_layer, LayerCollection *lc)
+{
+  if (lc->flag & LAYER_COLLECTION_EXCLUDE) {
+    return true;
+  }
+
+  /* Check visiblilty restriction flags */
+  if (lc->flag & LAYER_COLLECTION_HIDE || lc->collection->flag & COLLECTION_RESTRICT_VIEWPORT) {
+    return true;
+  }
+  else {
+    /* Restriction flags stay set, so we need to check parents */
+    CollectionParent *parent = lc->collection->parents.first;
+
+    if (parent) {
+      lc = BKE_layer_collection_first_from_scene_collection(view_layer, parent->collection);
+
+      return layer_collection_hidden(view_layer, lc);
+    }
+    else {
+      return false;
+    }
+  }
+
+  return false;
+}
+
+/**
  * Get the collection for a given index
  */
 LayerCollection *BKE_layer_collection_from_index(ViewLayer *view_layer, const int index)
@@ -537,7 +569,7 @@ LayerCollection *BKE_layer_collection_activate_parent(ViewLayer *view_layer, Lay
     lc = NULL;
   }
 
-  if (lc && (lc->flag & LAYER_COLLECTION_EXCLUDE)) {
+  if (lc && layer_collection_hidden(view_layer, lc)) {
     /* Don't activate excluded collections. */
     return BKE_layer_collection_activate_parent(view_layer, lc);
   }
@@ -816,8 +848,7 @@ void BKE_layer_collection_sync(const Scene *scene, ViewLayer *view_layer)
 
   /* Always set a valid active collection. */
   LayerCollection *active = view_layer->active_collection;
-
-  if (active && (active->flag & LAYER_COLLECTION_EXCLUDE)) {
+  if (active && layer_collection_hidden(view_layer, active)) {
     BKE_layer_collection_activate_parent(view_layer, active);
   }
   else if (active == NULL) {
