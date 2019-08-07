22fea4852f342da6240c927275a7ff7c4f00a25b
Nathan Craddock <nzcraddock@gmail.com>
2019-08-06 23:29:34 -0600

    Only notify on syncing specific types of data

--- source/blender/editors/space_outliner/outliner_sync.c
+++ source/blender/editors/space_outliner/outliner_sync.c
@@ -257,6 +257,8 @@ void ED_outliner_select_sync_from_outliner(bContext *C, SpaceOutliner *soops)
 
   /* Set global sync select flag based on outliner selection type */
   if (sync_types.object) {
+    DEG_id_tag_update(&scene->id, ID_RECALC_SELECT);
+    WM_event_add_notifier(C, NC_SCENE | ND_OB_SELECT, scene);
     ED_outliner_select_sync_from_object_tag(C);
   }
   if (sync_types.edit_bone) {
@@ -266,17 +268,13 @@ void ED_outliner_select_sync_from_outliner(bContext *C, SpaceOutliner *soops)
     ED_outliner_select_sync_from_pose_bone_tag(C);
   }
   if (sync_types.sequence) {
+    WM_event_add_notifier(C, NC_SCENE | ND_SEQUENCER | NA_SELECTED, scene);
     ED_outliner_select_sync_from_sequence_tag(C);
   }
 
   /* Flag all outliners as dirty except for self */
   ED_outliner_select_sync_flag_outliners(C);
   soops->sync_select_dirty &= ~WM_OUTLINER_SYNC_SELECT_FROM_ALL;
-
-  /* Update editors */
-  DEG_id_tag_update(&scene->id, ID_RECALC_SELECT);
-  WM_event_add_notifier(C, NC_SCENE | ND_OB_SELECT, scene);
-  WM_event_add_notifier(C, NC_SCENE | ND_SEQUENCER | NA_SELECTED, scene);
 }
 
 static void outliner_select_sync_from_object(ViewLayer *view_layer,
