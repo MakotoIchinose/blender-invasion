dd370cbf8956d5d1cde0c5e42251c9dbf93d16d8
Antonioya <blendergit@gmail.com>
2019-08-13 22:51:37 +0200

    GPencil: Increase number of input samples to 50
    For testing new brushes.

--- source/blender/makesdna/DNA_gpencil_types.h
+++ source/blender/makesdna/DNA_gpencil_types.h
@@ -36,7 +36,7 @@ struct MDeformVert;
 
 #define GP_DEFAULT_PIX_FACTOR 1.0f
 #define GP_DEFAULT_GRID_LINES 4
-#define GP_MAX_INPUT_SAMPLES 10
+#define GP_MAX_INPUT_SAMPLES 50
 
 /* ***************************************** */
 /* GP Stroke Points */
