599d0611b071cc0b1d5104ef437d79ce94b752d1
Julian Eisel <eiseljulian@gmail.com>
2019-08-22 13:33:06 +0200

    Improve batch file text for starting Blender with Oculus support

--- release/windows/batch/blender_oculus.cmd
+++ release/windows/batch/blender_oculus.cmd
@@ -1,9 +1,7 @@
 @echo off
-echo Starting blender with oculus openXR support, this assumes the oculus runtime
-echo is installed in the default location, if this is not the case please adjust the
-echo path inside oculus.josn
-echo.
-echo please note that openXR support is EXTREMELY experimental at this point
+echo Starting Blender with Oculus OpenXR support. This assumes the Oculus runtime
+echo is installed in the default location. If this is not the case, please adjust
+echo the path inside oculus.json.
 echo.
 pause
 set XR_RUNTIME_JSON=%~dp0oculus.json
