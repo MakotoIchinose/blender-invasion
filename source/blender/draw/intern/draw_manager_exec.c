d5555e5d5a20bfa373b6fe5e46da66bb7b09a69c
Clément Foucault <foucault.clem@gmail.com>
2019-08-29 19:23:43 +0200

    DRW: Add line offset to DRW_STATE_SHADOW_OFFSET
    This is needed for hairs.

--- source/blender/draw/intern/draw_manager_exec.c
+++ source/blender/draw/intern/draw_manager_exec.c
@@ -285,11 +285,13 @@ void drw_state_set(DRWState state)
     if ((test = CHANGED_TO(DRW_STATE_SHADOW_OFFSET))) {
       if (test == 1) {
         glEnable(GL_POLYGON_OFFSET_FILL);
+        glEnable(GL_POLYGON_OFFSET_LINE);
         /* 2.0 Seems to be the lowest possible slope bias that works in every case. */
         glPolygonOffset(2.0f, 1.0f);
       }
       else {
         glDisable(GL_POLYGON_OFFSET_FILL);
+        glDisable(GL_POLYGON_OFFSET_LINE);
       }
     }
   }
