aa0db0038ed82066db531938d660de730f987117
Julian Eisel <eiseljulian@gmail.com>
2019-09-02 16:14:07 +0200

    Fix empty tool settings region with old files
    E.g. visible in the VSE workspace.
    
    Also removes redundant NULL checks (do_versions_find_region asserts, and
    AFAIK they should never be NULL)

--- source/blender/blenloader/intern/versioning_280.c
+++ source/blender/blenloader/intern/versioning_280.c
@@ -3720,13 +3720,21 @@ void blo_do_versions_280(FileData *fd, Library *UNUSED(lib), Main *bmain)
             ListBase *regionbase = (sl == sa->spacedata.first) ? &sa->regionbase : &sl->regionbase;
             ARegion *ar_ui = do_versions_find_region(regionbase, RGN_TYPE_UI);
             ARegion *ar_header = do_versions_find_region(regionbase, RGN_TYPE_HEADER);
+            ARegion *ar_toolprops = do_versions_find_region(regionbase, RGN_TYPE_TOOL_PROPS);
 
-            if (ar_ui && ar_header) {
-              /* Reinsert UI region so that it spawns entire area width */
-              BLI_remlink(regionbase, ar_ui);
-              BLI_insertlinkafter(regionbase, ar_header, ar_ui);
+            /* Reinsert UI region so that it spawns entire area width */
+            BLI_remlink(regionbase, ar_ui);
+            BLI_insertlinkafter(regionbase, ar_header, ar_ui);
 
-              ar_ui->flag |= RGN_FLAG_DYNAMIC_SIZE;
+            ar_ui->flag |= RGN_FLAG_DYNAMIC_SIZE;
+
+            if (ar_toolprops->alignment == (RGN_ALIGN_BOTTOM | RGN_SPLIT_PREV)) {
+              SpaceType *stype = BKE_spacetype_from_id(sl->spacetype);
+
+              /* Remove empty region at old location. */
+              BLI_assert(sfile->op == NULL);
+              BKE_area_region_free(stype, ar_toolprops);
+              BLI_freelinkN(regionbase, ar_toolprops);
             }
 
             if (sfile->params) {
