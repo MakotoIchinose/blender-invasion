5db6ef594e96dde71071a216e3f0f314e54c70c3
mano-wii <germano.costa@ig.com.br>
2019-09-04 19:25:17 -0300

    Transform: Rearrange `transform_conversions` header

--- source/blender/editors/transform/transform_conversions.h
+++ source/blender/editors/transform/transform_conversions.h
@@ -34,7 +34,35 @@ struct TransDataContainer;
 struct TransDataCurveHandleFlags;
 struct TransInfo;
 
-/* helper struct for gp-frame transforms (only used here) */
+/* transform_conversions.c */
+
+/* Used by `transform_gizmo_3d.c` */
+int count_set_pose_transflags(Object *ob,
+                              const int mode,
+                              const short around,
+                              bool has_translate_rotate[2]);
+
+/* Used by `transform_generics.c` */
+void flushTransIntFrameActionData(TransInfo *t);
+void flushTransGraphData(TransInfo *t);
+void remake_graph_transdata(TransInfo *t, struct ListBase *anim_data);
+void flushTransUVs(TransInfo *t);
+void flushTransParticles(TransInfo *t);
+void flushTransNodes(TransInfo *t);
+void flushTransSeq(TransInfo *t);
+void flushTransTracking(TransInfo *t);
+void flushTransMasking(TransInfo *t);
+void flushTransPaintCurve(TransInfo *t);
+void restoreMirrorPoseBones(TransDataContainer *tc);
+void restoreBones(TransDataContainer *tc);
+
+/* Used by `transform.c` */
+bool clipUVTransform(TransInfo *t, float vec[2], const bool resize);
+void clipUVData(TransInfo *t);
+
+/********************* intern **********************/
+
+/* helper struct for gp-frame transforms */
 typedef struct tGPFtransdata {
   float val;  /* where transdata writes transform */
   int *sdata; /* pointer to gpf->framenum */
@@ -45,32 +73,12 @@ typedef struct TransDataGraph {
   float offset;
 } TransDataGraph;
 
-/* transform_conversions.c */
 void transform_around_single_fallback(TransInfo *t);
-int count_set_pose_transflags(Object *ob,
-                              const int mode,
-                              const short around,
-                              bool has_translate_rotate[2]);
 bool constraints_list_needinv(TransInfo *t, ListBase *list);
 void calc_distanceCurveVerts(TransData *head, TransData *tail);
 struct TransDataCurveHandleFlags *initTransDataCurveHandles(TransData *td, struct BezTriple *bezt);
 bool FrameOnMouseSide(char side, float frame, float cframe);
 void clear_trans_object_base_flags(TransInfo *t);
-
-void flushTransIntFrameActionData(TransInfo *t);
-void flushTransGraphData(TransInfo *t);
-void remake_graph_transdata(TransInfo *t, struct ListBase *anim_data);
-void flushTransUVs(TransInfo *t);
-void flushTransParticles(TransInfo *t);
-bool clipUVTransform(TransInfo *t, float vec[2], const bool resize);
-void clipUVData(TransInfo *t);
-void flushTransNodes(TransInfo *t);
-void flushTransSeq(TransInfo *t);
-void flushTransTracking(TransInfo *t);
-void flushTransMasking(TransInfo *t);
-void flushTransPaintCurve(TransInfo *t);
-void restoreMirrorPoseBones(TransDataContainer *tc);
-void restoreBones(TransDataContainer *tc);
 
 /* transform_conversions_actiondata.c */
 void createTransActionData(bContext *C, TransInfo *t);
@@ -108,9 +116,14 @@ void createTransUVs(bContext *C, TransInfo *t);
 /* transform_conversions_nla.c */
 void createTransNlaData(bContext *C, TransInfo *t);
 
+/* transform_conversions_node.c */
+void createTransNodeData(bContext *UNUSED(C), TransInfo *t);
+
 /* transform_object.c */
 void createTransObject(bContext *C, TransInfo *t);
 void createTransTexspace(TransInfo *t);
+
+/* Used by `transform_generics.c` (Should be in `transform_conversion.c`) */
 void trans_obdata_in_obmode_update_all(struct TransInfo *t);
 void trans_obchild_in_obmode_update_all(struct TransInfo *t);
 
