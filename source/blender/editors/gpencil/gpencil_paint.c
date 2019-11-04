1243aec8bc1411e762c3786c1cb595e21e9595a5
Antonio Vazquez <blendergit@gmail.com>
2019-11-04 18:05:36 +0100

    GPencil: Cleanup

--- source/blender/editors/gpencil/gpencil_paint.c
+++ source/blender/editors/gpencil/gpencil_paint.c
@@ -973,12 +973,6 @@ static void gp_stroke_newfrombuffer(tGPsdata *p)
   gps->inittime = p->inittime;
   gps->uv_scale = 1.0f;
 
-  /* Apply the mix color to fill. */
-  if (GPENCIL_USE_VERTEX_COLOR_FILL(ts)) {
-    copy_v3_v3(gps->mix_color_fill, brush->rgb);
-    gps->mix_color_fill[3] = brush->gpencil_settings->vertex_factor;
-  }
-
   /* enable recalculation flag by default */
   gps->flag |= GP_STROKE_RECALC_GEOMETRY;
 
@@ -998,6 +992,12 @@ static void gp_stroke_newfrombuffer(tGPsdata *p)
   pt = gps->points + (gps->totpoints - totelem);
   if (gps->dvert != NULL) {
     dvert = gps->dvert + (gps->totpoints - totelem);
+  }
+
+  /* Apply the mix color to fill for stroke. */
+  if (GPENCIL_USE_VERTEX_COLOR_FILL(ts)) {
+    copy_v3_v3(gps->mix_color_fill, brush->rgb);
+    gps->mix_color_fill[3] = brush->gpencil_settings->vertex_factor;
   }
 
   /* copy points from the buffer to the stroke */
