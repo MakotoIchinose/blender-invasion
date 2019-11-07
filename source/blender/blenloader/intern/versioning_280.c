6a5fa02cb9d9c9eebc0a43d91d357578a8f3bda8
Antonio Vazquez <blendergit@gmail.com>
2019-11-07 14:00:51 +0100

    GPencil: Add versioning code for new tool

--- source/blender/blenloader/intern/versioning_280.c
+++ source/blender/blenloader/intern/versioning_280.c
@@ -3967,5 +3967,15 @@ void blo_do_versions_280(FileData *fd, Library *UNUSED(lib), Main *bmain)
         sa->flag &= ~AREA_FLAG_UNUSED_6;
       }
     }
+
+    /* Init new Grease Pencil Vertex Paint tools. */
+    {
+      for (Brush *brush = bmain->brushes.first; brush; brush = brush->id.next) {
+        if (brush->gpencil_settings != NULL) {
+          brush->gpencil_vertex_tool = brush->gpencil_settings->brush_type;
+        }
+      }
+      BKE_paint_toolslots_init_from_main(bmain);
+    }
   }
 }
