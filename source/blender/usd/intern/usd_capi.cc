dd71c97e71714c286ad94488928fa8dc88da2c68
Sybren A. Stüvel <sybren@blender.org>
2019-11-22 13:03:20 +0100

    USD: Added & corrected GPL license blocks

--- source/blender/usd/intern/usd_capi.cc
+++ source/blender/usd/intern/usd_capi.cc
@@ -12,10 +12,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- */
-
-/** \file
- * \ingroup USD
+ *
+ * The Original Code is Copyright (C) 2019 Blender Foundation.
+ * All rights reserved.
  */
 
 #include "../usd.h"
