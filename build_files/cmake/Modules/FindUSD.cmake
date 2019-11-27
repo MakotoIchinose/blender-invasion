75bed0dfa97a4cb1c1b313de1502321aa099b45e
Sybren A. Stüvel <sybren@blender.org>
2019-11-27 17:25:37 +0100

    USD: Fixed static library linking on Linux
    This may have inadvertently broken things on Windows, I'll work on a better
    way soon.

--- build_files/cmake/Modules/FindUSD.cmake
+++ build_files/cmake/Modules/FindUSD.cmake
@@ -48,7 +48,7 @@ FIND_PATH(USD_INCLUDE_DIR
     include
 )
 
-FIND_LIBRARY(USD_LIBRARY
+FIND_LIBRARY(_usd_LIBRARY
   NAMES
   usd
   HINTS
@@ -60,18 +60,26 @@ FIND_LIBRARY(USD_LIBRARY
 # handle the QUIETLY and REQUIRED arguments and set USD_FOUND to TRUE if
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(USD DEFAULT_MSG USD_ROOT_DIR USD_LIBRARY USD_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(USD DEFAULT_MSG USD_ROOT_DIR _usd_LIBRARY USD_INCLUDE_DIR)
 
 IF(USD_FOUND)
-  SET(USD_LIBRARIES ${USD_LIBRARY})
+  get_filename_component(USD_LIBRARY_DIR ${_usd_LIBRARY} DIRECTORY)
+
+  SET(USD_LIBRARIES)
+  # List copied from pxrTargets.cmake, which is created when building USD.
+  foreach(_usd_lib arch tf gf js trace work plug vt ar kind sdf ndr sdr pcp usd usdGeom usdVol usdLux usdShade usdRender usdHydra usdRi usdSkel usdUI usdUtils)
+    list(APPEND USD_LIBRARIES "${USD_LIBRARY_DIR}/lib${_usd_lib}.a")
+  endforeach()
+  list(REVERSE USD_LIBRARIES)
+
   SET(USD_INCLUDE_DIRS ${USD_INCLUDE_DIR})
-  SET(USD_ROOT_DIR ${USD_ROOT_DIR})
 ENDIF(USD_FOUND)
 
 MARK_AS_ADVANCED(
   USD_INCLUDE_DIR
-  USD_LIBRARY
+  USD_LIBRARY_DIR
   USD_ROOT_DIR
 )
 
+UNSET(_usd_LIBRARY)
 UNSET(_usd_SEARCH_DIRS)
