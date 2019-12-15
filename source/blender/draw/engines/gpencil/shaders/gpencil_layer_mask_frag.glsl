0edf755317914a287ac2570b12e933b58b750ce1
Clément Foucault <foucault.clem@gmail.com>
2019-12-15 23:55:40 +0100

    GPencil: Refactor: Fix mask system not working in some scenes

--- source/blender/draw/engines/gpencil/shaders/gpencil_layer_mask_frag.glsl
+++ source/blender/draw/engines/gpencil/shaders/gpencil_layer_mask_frag.glsl
@@ -7,7 +7,8 @@ uniform bool isFirstPass;
 
 in vec4 uvcoordsvar;
 
-out vec4 fragColor;
+layout(location = 0) out vec4 fragColor;
+layout(location = 1) out vec4 fragRevealage;
 
 void main()
 {
@@ -17,11 +18,13 @@ void main()
 
   if (isFirstPass) {
     /* Blend mode is multiply. */
-    fragColor.rgb = mix(vec3(1.0), masked_reveal, mask);
-    fragColor.a = 1.0;
+    fragColor = vec4(1.0);
+    fragRevealage.rgb = mix(vec3(1.0), masked_reveal, mask);
+    fragRevealage.a = 1.0;
   }
   else {
     /* Blend mode is additive. */
+    fragRevealage = vec4(0.0);
     fragColor.rgb = mix(vec3(0.0), masked_color, mask);
     fragColor.a = 0.0;
   }
