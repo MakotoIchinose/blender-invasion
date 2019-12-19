96a3029ab9f0bb977cbc87d0853e542dd7ae5a5d
Antonio Vazquez <blendergit@gmail.com>
2019-12-19 08:55:41 +0100

    GPencil: Fix missing mask icon in Dopesheet
    The new mask icon was not displayed in Dopesheet channel.

--- source/blender/editors/animation/anim_channels_defines.c
+++ source/blender/editors/animation/anim_channels_defines.c
@@ -5092,7 +5092,15 @@ void ANIM_channel_draw_widgets(const bContext *C,
           prop = RNA_struct_find_property(&ptr, "mask_layer");
           gp_rna_path = RNA_path_from_ID_to_property(&ptr, prop);
           if (RNA_path_resolve_property(&id_ptr, gp_rna_path, &ptr, &prop)) {
-            icon = (gpl->flag & GP_LAYER_USE_MASK) ? ICON_MOD_MASK : ICON_LAYER_ACTIVE;
+            icon = ICON_LAYER_ACTIVE;
+            if (gpl->flag & GP_LAYER_USE_MASK) {
+              if (gpl->flag & GP_LAYER_MASK_INVERT) {
+                icon = ICON_HOLDOUT_ON;
+              }
+              else {
+                icon = ICON_MOD_MASK;
+              }
+            }
             uiDefAutoButR(block,
                           &ptr,
                           prop,
